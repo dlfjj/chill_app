bb0c19abbcfd31757458eb11005862e0f5e38232
dlfjj <dlfjj123@gmail.com>
2019-04-29 23:33:12 -0700

    billionair idea

---
+++ components/RandomChart.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "RandomChart"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/custom-item.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "custome-item"
+    }
+</script>
+
+<style scoped>
+
+</style>
